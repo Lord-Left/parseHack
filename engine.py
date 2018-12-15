2e59483945a923d94f766bee171606949de7c5a2
Christopher <sethosayher@gmail.com>
2018-12-15 14:57:45 -0500

    Updated engine.py with logic to render main char

--- engine.py
+++ engine.py
@@ -1,7 +1,21 @@
-import libtcodpy as libtcod
+import libtcodpy as lt
 
 def main():
-	print('Hello World')
+	screen_width = 80
+	screen_height = 50
+
+	lt.console_set_custom_font('arial10x10.png', lt.FONT_TYPE_GREYSCALE | lt.FONT_LAYOUT_TCOD)
+
+	lt.console_init_root(screen_width, screen_height, 'PARSE_HACK 0.0.1', False)
+	while not lt.console_is_window_closed():
+		lt.console_set_default_foreground(0,lt.white)
+		lt.console_put_char(0,1,1,'@',lt.BKGND_NONE)
+		lt.console_flush()
+
+		key = lt.console_check_for_keypress()
+
+		if key.vk == lt.KEY_ESCAPE:
+			return True
 
 if __name__ == '__main__':
 	main()
